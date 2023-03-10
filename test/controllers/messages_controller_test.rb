5466c1df4784af47fc076d18b378f8c9443d2ded
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-03-10 21:43:16 +0100

    Fix linter errors

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get messages_index_url
     assert_response :success
   end
